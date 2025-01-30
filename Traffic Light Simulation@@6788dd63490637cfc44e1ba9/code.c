517d336583ab54a6276b1439aaabf2765ea45200
vanshambansal <vanshambansal1210@gmail.com>
2025-01-30 10:45:57 +0530

    Solution to Traffic Light Simulation

---
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    char ch;
+    scanf("%c",&ch);
+    a=='R'?printf("Stop"):
+    a=='G'?printf("Go"):
+    a=='Y'?printf("Slow Down"):
+    printf("Invalid input");
+    return 0;
+}
