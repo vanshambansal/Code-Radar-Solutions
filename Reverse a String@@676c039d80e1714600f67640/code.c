5ba0f23cccd2bea68d947dedf5a23cffb7574456
vanshambansal <vanshambansal1210@gmail.com>
2025-02-04 11:58:36 +0530

    Solution to Reverse a String

---
+++ Reverse a String@@676c039d80e1714600f67640/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    char s[100];
+    scanf("%s",&s);
+    printf("%s",strrev(s));
+
+
+    return 0;
+}
