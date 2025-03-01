fc272708e937890fbf6040ec690c3b3fc86b9f7e
Vansham Bansal <vanshambansal1210@gmail.com>
2025-03-01 12:24:53 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -47,9 +47,15 @@ int main(){
         }
         printf("\n");
     }
-    // for(int i=1;i<=n-1;i++){
-    //     for(int i=)
-    // }
+    for(int i=1;i<=n-1;i++){
+        for(int j=1;j<=i;j++){
+            printf(" ");
+        }
+        for(int j=n-1;j>=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
 
 
 
