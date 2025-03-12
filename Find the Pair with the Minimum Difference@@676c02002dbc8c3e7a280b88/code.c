e074584ad53a0c453f46fe343b0c20de6d1a2536
Vansham Bansal <vanshambansal1210@gmail.com>
2025-03-12 11:55:28 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -11,7 +11,7 @@ int main(){
             if((arr[i]-arr[j])<min){
                 min = arr[i]-arr[j];
                 if(min<0){
-                    break;
+                    min=min*(-1);
                 }
             }
         }
