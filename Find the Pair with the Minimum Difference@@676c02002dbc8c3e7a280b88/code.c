20fab720e27f8cc0da6095b429ad73ac7a67bee4
Vansham Bansal <vanshambansal1210@gmail.com>
2025-03-12 12:48:47 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,83 +1,81 @@
-// #include<stdio.h>
-// int main(){
-//     int n,min=10000,a,b;
-//     scanf("%d",&n);
+#include<stdio.h>
+int main(){
+    int n,min=10000,a,b;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i+1 ; j<n;j++){
+            int diff  = arr[i]-arr[j];
+            if(diff<0){
+                    diff = -diff;
+                }
+            if(diff < min){
+                
+                min = diff; 
+                a = arr[i];
+                b= arr[j]; 
+            }
+        }
+    }
+  
+                if(a>b){
+                    printf("%d %d",arr[j],arr[i]);
+                }
+                else if (b>a){
+                    printf("%d %d",arr[i],arr[j]);
+                }else{
+                    printf("-1");
+                }
+                
+              
+    return 0;
+}
+
+
+
+
+
+
+
+
+// #include <stdio.h>
+// #include <limits.h>  // For INT_MAX
+
+// int main() {
+//     int n, min = INT_MAX, a, b;
+//     scanf("%d", &n);
 //     int arr[n];
-//     for(int i=0;i<n;i++){
-//         scanf("%d",&arr[i]);
+
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
 //     }
-//     for(int i=0;i<n;i++){
-//         for(int j=i+1 ; j<n;j++){
-//             int diff  = arr[i]-arr[j];
-//             if(diff<0){
-//                     diff = -diff;
-//                 }
-//             if(diff < min){
-                
-//                 min = diff;  
+
+//     // Finding the smallest absolute difference
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i + 1; j < n; j++) {
+//             int diff = arr[i] - arr[j];
+
+//             if (diff < 0) { // Convert negative difference to positive
+//                 diff = -diff;
+//             }
+
+//             if (diff < min) { // Update min difference
+//                 min = diff;
+//                 a = arr[i];
+//                 b = arr[j];
 //             }
 //         }
 //     }
-//     for(int i=0;i<n;i++){
-//         for(int j=i+1;j<n;j++){
-//             int diff  = arr[i]-arr[j];
-//             if(diff<0){
-//                     diff = -diff;
-//                 }
-//             if(diff == min){
-//                 if(arr[i]>arr[j]){
-//                     printf("%d %d",arr[j],arr[i]);
-//                 }
-//                 else{
-//                     printf("%d %d",arr[i],arr[j]);
-//                 }
-                
-//                return 0;
-//             }
-//         }
+
+//     // Printing the first pair with the smallest difference
+//     if (a > b) {  // ✅ Ensure smaller number comes first
+//         printf("%d %d\n", b, a);
+//     } else {
+//         printf("%d %d\n", a, b);
 //     }
 
 //     return 0;
 // }
-
-
-
-
-#include <stdio.h>
-#include <limits.h>  // For INT_MAX
-
-int main() {
-    int n, min = INT_MAX, a, b;
-    scanf("%d", &n);
-    int arr[n];
-
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    // Finding the smallest absolute difference
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            int diff = arr[i] - arr[j];
-
-            if (diff < 0) { // Convert negative difference to positive
-                diff = -diff;
-            }
-
-            if (diff < min) { // Update min difference
-                min = diff;
-                a = arr[i];
-                b = arr[j];
-            }
-        }
-    }
-
-    // Printing the first pair with the smallest difference
-    if (a > b) {  // ✅ Ensure smaller number comes first
-        printf("%d %d\n", b, a);
-    } else {
-        printf("%d %d\n", a, b);
-    }
-
-    return 0;
-}
