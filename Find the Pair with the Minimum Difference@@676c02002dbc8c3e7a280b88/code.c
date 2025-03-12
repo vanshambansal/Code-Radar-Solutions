3b3088689a7cf0800358dd3a1aef1082bcfa75cc
Vansham Bansal <vanshambansal1210@gmail.com>
2025-03-12 12:22:52 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -25,7 +25,7 @@ int main(){
                     diff = -diff;
                 }
             if(diff == min){
-                printf("%d %d",arr[i],arr[j]);
+                printf("%d %d",arr[j],arr[i]);
             }
         }
     }
