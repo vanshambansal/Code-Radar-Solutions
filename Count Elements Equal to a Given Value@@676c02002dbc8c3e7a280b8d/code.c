b17cc9d7218113a3592fbc7c8156be1e4adf62c7
Vansham Bansal <vanshambansal1210@gmail.com>
2025-03-12 10:54:11 +0530

    Solution to Count Elements Equal to a Given Value

---
+++ Count Elements Equal to a Given Value@@676c02002dbc8c3e7a280b8d/code.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n,k,count=0;
+    scanf("%d %d",&n,&k);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        if(arr[i]==k){
+            count++;
+        }
+    }
+    printf("%d",count);
+    
+
+    return 0;
+}
