6959a3aba41b899e2ba70c7f37688c4a02594818
Vansham Bansal <vanshambansal1210@gmail.com>
2025-03-19 12:43:52 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,7 +1,23 @@
-void caesarCipher(char message[] , int shift , char encrypted[]){
+#include <stdio.h>
+#include <string.h>
 
-    for(int i=0;i<shift;i++){
-       encrypted[i] =message[i]+shift;
+void caesarCipher(char message[], int shift, char encrypted[]) {
+    for (int i = 0; message[i] != '\0'; i++) {
+        char ch = message[i];
 
+        // Encrypt uppercase letters
+        if (ch >= 'A' && ch <= 'Z') {
+            encrypted[i] = ((ch - 'A' + shift) % 26) + 'A';
+        }
+        // Encrypt lowercase letters
+        else if (ch >= 'a' && ch <= 'z') {
+            encrypted[i] = ((ch - 'a' + shift) % 26) + 'a';
+        }
+        // Keep other characters unchanged (like spaces, punctuation)
+        else {
+            encrypted[i] = ch;
+        }
     }
-}   
+
+    encrypted[strlen(message)] = '\0'; // Null-terminate the encrypted string
+}
