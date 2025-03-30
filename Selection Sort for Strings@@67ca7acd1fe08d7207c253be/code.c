722f77f516177a6432cb4c6ba04788bb9a3381b6
Vansham Bansal <vanshambansal1210@gmail.com>
2025-03-30 21:46:35 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,50 +1,24 @@
-void selectionSort(char arr[][100],int n){
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                int k = 0;
-            while ( arr[i][k] == arr[j][k]) { 
+void selectionSort(char arr[][100], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int k = 0;
+            while (arr[i][k] == arr[j][k]) { 
                 k++;
             }
-                if(arr[i][k]>arr[j][k]){
-                    char* temp= arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+            if (strcmp(arr[i], arr[j]) > 0) { // ✅ Use strcmp for string comparison
+                char temp[100]; // ✅ Use a temporary string for swapping
+                strcpy(temp, arr[i]);  
+                strcpy(arr[i], arr[j]); 
+                strcpy(arr[j], temp);
             }
         }
-        }
-void printArray(char arr[][100], int n){
-    for(int i=0;i<n;i++){
-        printf("%s ",arr[i]);
     }
 }
 
-
-
-
-
-
-
-
-
-
-// #include <stdio.h>
-
-// int main() {
-//     int num1, num2, sum;
-//     int *ptr1, *ptr2;
-
-//     scanf("%d %d", &num1, &num2);
-
-    
-//     ptr1 = &num1;
-//     ptr2 = &num2;
-
-//     sum = *ptr1 + *ptr2;
-
-    
-//     printf("Sum = %d\n", sum);
-
-//     return 0;
-// }
-
+// Print function remains the same
+void printArray(char arr[][100], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%s ", arr[i]);
+    }
+    printf("\n");
+}
