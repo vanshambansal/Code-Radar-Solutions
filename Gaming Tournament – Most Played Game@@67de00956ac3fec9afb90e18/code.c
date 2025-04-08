e7e813619e544d64526c3bc15f774b555b30def2
Vansham Bansal <vanshambansal1210@gmail.com>
2025-04-08 14:15:27 +0530

    Solution to Gaming Tournament – Most Played Game

--- Gaming Tournament – Most Played Game@@67de00956ac3fec9afb90e18/code.c
+++ Gaming Tournament – Most Played Game@@67de00956ac3fec9afb90e18/code.c
@@ -12,7 +12,7 @@ int mostPlayedGame(int arr[] ,  int n){
 
     for(int i=0;i<n;i++){
         int count =0;
-        for(int j=i+1;j<n;j++){
+        for(int j=0;j<n;j++){
             if(arr[i]==arr[j]){
                 count++;
             }
