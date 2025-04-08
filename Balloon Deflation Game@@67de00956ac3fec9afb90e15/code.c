d7934168c46fe5c77d9637732ba58e08bdf0940b
Vansham Bansal <vanshambansal1210@gmail.com>
2025-04-08 12:20:56 +0530

    Solution to Balloon Deflation Game

--- Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -1,26 +1,30 @@
+#include <stdio.h>
+
 void deflateBalloons(int arr[], int n) {
-    while(1){
+    while (1) {
+        int min = -1;
+        int count = 0;
 
-        int min=-1;
-        int count =0;
-        for(int i=0;i<n;i++){
-            if(arr[i] > 0 && (min == -1 || arr[i] < min)){
-                min=arr[i];
+        // Find minimum non-zero value
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0 && (min == -1 || arr[i] < min)) {
+                min = arr[i];
             }
         }
-        for(int i=0;i<n;i++){
-            if(arr[i]>0){
-                arr[i]-=min;
-                count++;
 
-            }
-
-        }
-        if(count == 0) {
+        // If all values are 0, break the loop
+        if (min == -1) {
             break;
         }
-        printf("%d\n",count);
-        
 
+        // Subtract min from all positive values and count how many are non-zero
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                arr[i] -= min;
+                count++;
+            }
+        }
+
+        printf("%d\n", count);
     }
 }
