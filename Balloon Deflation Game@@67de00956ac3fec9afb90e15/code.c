1170de197d7c3ad31c3c412a6a8257a7bac8aa3e
Vansham Bansal <vanshambansal1210@gmail.com>
2025-04-08 12:15:18 +0530

    Solution to Balloon Deflation Game

--- Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -4,7 +4,7 @@ void deflateBalloons(int arr[], int n) {
         int min=1000;
         int count =0;
         for(int i=0;i<n;i++){
-            if(arr[i]<min && arr[i]>0){
+            if(arr[i]<min && arr[i]>=0){
                 min=arr[i];
             }
         }
