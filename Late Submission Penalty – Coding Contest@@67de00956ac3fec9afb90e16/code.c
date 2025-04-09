69e06b4e5c676a1dd2caac7bb968c3450569ef7c
Vansham Bansal <vanshambansal1210@gmail.com>
2025-04-09 09:27:18 +0530

    Solution to Late Submission Penalty – Coding Contest

---
+++ Late Submission Penalty – Coding Contest@@67de00956ac3fec9afb90e16/code.c
@@ -0,0 +1,13 @@
+int calculatePenalty(int d , int m , int y , int dd , int dm , int dy){
+    if((dy - y)!=0){
+        return 5000;
+    }
+    else if((dm - m)!=0){
+        int ans = 200 * (dm - m);
+        return ans;
+    }
+    else{
+        int ans1 = 10 * (dd - d);
+        return ans1;
+    }
+}
