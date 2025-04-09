eea92dcfc12d463991f0059dd10d12ddce69259c
Vansham Bansal <vanshambansal1210@gmail.com>
2025-04-09 10:05:32 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

---
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -0,0 +1,13 @@
+void trackPLayerRanks(int ranked[] , int n , int played[] , int m , int result[]){
+
+    for(int i=0;i<m;i++){
+        int count =0;
+        for(int j=0 ; j<n ; j++){
+            if(played[i]<ranked[j]){
+                count ++;
+            }
+        }
+        result[i]=count;
+    }
+
+}
